ecec57fad2634ff5139b292da89d3272c3b53ab7
Panatchai Vathanasri <vathanasri.panatchai@crypto.com>
2022-06-09 15:36:27 +0700

    first commit

---
+++ app/src/main/java/com/example/testreflection/OldClass.kt
@@ -0,0 +1,31 @@
+package com.example.testreflection
+
+class OldClass {
+    private val s = ""
+
+    fun method1() {
+    }
+
+    fun method2() {
+    }
+
+    fun method3() {
+    }
+
+    fun OldClass.eFunc(): Int = 0
+}
+
+class NewClass {
+    private val s = ""
+
+    fun method1() {
+    }
+
+    fun method2() {
+    }
+
+    fun method3() {
+    }
+
+    fun NewClass.eFunc(): Int = 0
+}
---
+++ app/src/test/java/com/example/testreflection/OldClassTest.kt
@@ -0,0 +1,5 @@
+package com.example.testreflection
+
+import org.junit.Assert.*
+
+class OldClassTest
